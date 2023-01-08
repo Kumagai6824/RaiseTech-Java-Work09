c2b00d3f5528bd94a242c753e4274a87c104f42d
Kumagai-java2211 <j.min.howsand682@gmail.com>
2023-01-08 16:45:25 +0900

    MyBatisを使ったREAD処理を実装

---
+++ src/main/java/com/raisetech/work09/Name.java
@@ -0,0 +1,19 @@
+package com.raisetech.work09;
+
+public class Name {
+    private int id;
+    private String name;
+
+    public Name(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
