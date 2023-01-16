ef46ee3839464da4462fac7d433747c44967007b
Kumagai-java2211 <j.min.howsand682@gmail.com>
2023-01-16 22:55:45 +0900

    findByIdの実装

--- src/main/java/com/raisetech/work09/NameController.java
+++ src/main/java/com/raisetech/work09/NameController.java
@@ -1,11 +1,17 @@
 package com.raisetech.work09;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Validated
 public class NameController {
 
     private final NameService nameService;
@@ -17,5 +23,16 @@ public class NameController {
     @GetMapping("/names")
     public List<Name> getNames() {
         return nameService.findAll();
+    }
+
+    @GetMapping("/names/find/{id}")
+    public Name findById(
+            @PathVariable(value = "id")
+            @Valid
+            @NotNull
+            @Min(1)
+            int id) throws Exception {
+        return nameService.findById(id);
+
     }
 }
--- src/main/java/com/raisetech/work09/NameService.java
+++ src/main/java/com/raisetech/work09/NameService.java
@@ -1,10 +1,9 @@
 package com.raisetech.work09;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
 public interface NameService {
     List<Name> findAll();
+
+    Name findById(int id) throws Exception;
 }
