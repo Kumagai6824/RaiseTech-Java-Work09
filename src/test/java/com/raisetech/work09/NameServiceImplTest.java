c3f85397ee3784b77834f35f28f012e5d66d084b
Kumagai-java2211 <j.min.howsand682@gmail.com>
2023-02-27 20:41:01 +0900

    [Modified]存在しないユーザーIDを指定したときに期待通り例外を返すことテストの修正

--- src/test/java/com/raisetech/work09/NameServiceImplTest.java
+++ src/test/java/com/raisetech/work09/NameServiceImplTest.java
@@ -9,9 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 class NameServiceImplTest {
@@ -30,20 +29,10 @@ class NameServiceImplTest {
     }
 
     @Test
-    public void 存在しないユーザーIDを指定したときに期待通り例外を返すこと() throws Exception {
-        doThrow(new ResourceNotFoundException("例外です")).when(nameMapper).findById(0);
-        Throwable throwable = catchThrowable(() -> nameServiceImpl.findById(0));
-        /*catchThrowableを使うことで、与えられたコードブロックを実行し、
-        その中でスローされた例外をキャッチして返す、若しくは例外がスローされなかったらnullを返す
-        */
-        assertThat(throwable).isInstanceOf(ResourceNotFoundException.class).hasMessage("例外です");
-        /*assertThatThrownByで試したが、アサーションエラーが発生した。
-        同メソッドが期待するような形式で例外をラップできていなかった可能性があり、
-        isInstanceOfメソッドを使用して、スローされた例外が期待通りの型であることを確認することになった。
-         */
-        assertThat(throwable.getCause()).isNull();
-        /*ResourceNotFoundExceptionは通常別の例外が原因となってスローされるケースがあるので
-        その原因となる別の例外が存在しないことを検証している。
-        その別の例外が存在する場合、getCause()はその例外を返すため、isNull()は失敗する*/
+    public void 存在しないユーザーIDを指定したときに期待通り例外を返すこと() {
+        doReturn(Optional.empty()).when(nameMapper).findById(0);
+        assertThatThrownBy(() -> nameServiceImpl.findById(0))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("resource not found with id: 0");
     }
 }
